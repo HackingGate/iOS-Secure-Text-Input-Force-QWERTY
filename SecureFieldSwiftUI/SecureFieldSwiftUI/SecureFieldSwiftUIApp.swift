2073058eaa48461e57571732586e9ae8d1561b20
HackingGate <i@hackinggate.com>
2021-01-27 21:40:15 +0900

    Init iOS SwiftUI project

---
+++ SecureFieldSwiftUI/SecureFieldSwiftUI/SecureFieldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureFieldSwiftUIApp.swift
+//  SecureFieldSwiftUI
+//
+//  Created by HG on 2021/01/27.
+//
+
+import SwiftUI
+
+@main
+struct SecureFieldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
